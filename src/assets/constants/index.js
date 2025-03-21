d6d302254d8c014364e36e3081a12a51ced59ddf
martinasweety <149145751+martinasweety@users.noreply.github.com>
2025-03-21 12:13:07 +0530

    Update index.js

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,12 +1,19 @@
 import project1 from "../images/image-search-engine.webp";
 import project2 from "../images/Text-to-voice.png";
 import project3 from "../images/weather-app.jpg";
+import project4 from "../images/ecommer.jpg";
 
 export const HERO_CONTENT = `Im a fresher Python Full Stack Developer, I am eager to apply my strong foundation in both front-end and back-end development to create dynamic and scalable web applications. I have a solid understanding of Python and frameworks like Django and Flask for back-end development, along with proficiency in HTML, CSS, JavaScript and React.js for building interactive front-end interfaces. Additionally, I have worked with MySQL to manage and interact with databases effectively.`;
 
 export const ABOUT_TEXT = `I am a passionate fresher Python Full Stack Developer with a strong foundation in Python, Django, and Flask for back-end development, and HTML, CSS, JavaScript, and React for front-end development. I am proficient in working with database in MySQL. Eager to learn and grow, I’m excited to apply my skills to build dynamic, scalable web applications and contribute to impactful projects.`;
 
 export const ProjectData = [
+  {
+    Image: project4,
+    Title: " Elite Collections E-Commerce Website",
+    Description: "A React E-Commerce Website is a dynamic web application that enables users to browse, add to cart, and purchase products with features like authentication, payment integration, and state management.",
+    Link: "https://elitecollections-ecommerce-website.netlify.app/"
+  },
   {
     Image: project1,
     Title: "Image Search Engine",
@@ -24,7 +31,7 @@ export const ProjectData = [
     Title: "Weather App",
     Description: "A Weather App using HTML, CSS, and JavaScript allows users to check real-time weather information based on their location or a specified city.",
     Link: "https://weather-app-sweety.netlify.app/"
-  },
+  },  
 ];
 
 export const CONTACT = {
